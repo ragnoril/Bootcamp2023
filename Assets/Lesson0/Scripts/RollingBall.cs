73ddf308d7d6a8c42263a1b2bacac359a44d044d
ragnoril <ozanemirhan@gmail.com>
2023-09-18 19:49:59 +0300

    commit for the first lesson

---
+++ Assets/Lesson0/Scripts/RollingBall.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBall : MonoBehaviour
+{
+    public float Speed;
+    public float JumpSpeed;
+    private Rigidbody rBody;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, z) * Time.fixedDeltaTime * Speed;
+        movement.y = rBody.velocity.y;
+
+        rBody.velocity = movement;
+        //rb.AddForce(movement);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rBody.AddForce(Vector3.up * JumpSpeed, ForceMode.Impulse);
+        }
+
+    }
+
+    
+
+
+}
