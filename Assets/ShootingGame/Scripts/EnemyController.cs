9a3827ce9c81d9c3619c2f5e80dfb0533875729f
ragnoril <ozanemirhan@gmail.com>
2023-09-22 20:01:29 +0300

    lesson #3

--- Assets/ShootingGame/Scripts/EnemyController.cs
+++ Assets/ShootingGame/Scripts/EnemyController.cs
@@ -1,4 +1,5 @@
 using ShooterGame;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.PackageManager;
@@ -7,6 +8,7 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private Rigidbody _rigidBody;
+    private Animator _animator;
 
     [SerializeField]
     private float _moveSpeed;
@@ -19,17 +21,44 @@ public class EnemyController : MonoBehaviour
 
     private float _currentHealth;
 
+    [SerializeField]
+    private float _attackRate;
+    private float _attackTimer;
+
+    private bool _canAttack;
+
 
     private void Start()
     {
+        _animator = GetComponent<Animator>();
         _rigidBody = GetComponent<Rigidbody>();
         _target = GameObject.Find("Player").GetComponent<PlayerController>();
 
         _currentHealth = _maxHealth;
+        _attackTimer = 0f;
+
+        _canAttack = false;
+    }
+
+    private void Update()
+    {
+        _attackTimer += Time.deltaTime;
+
+        if ( _canAttack && _attackTimer > _attackRate)
+        {
+            Attack();
+        }
     }
 
     private void FixedUpdate()
     {
+        if (_canAttack)
+        {
+            _rigidBody.velocity = Vector3.zero;
+            _rigidBody.angularVelocity = Vector3.zero;
+            return;
+        }
+
         float angleBetween = 270f - Mathf.Atan2(transform.position.z - _target.transform.position.z, transform.position.x - _target.transform.position.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, angleBetween, 0);
 
@@ -43,9 +72,35 @@ public class EnemyController : MonoBehaviour
             _currentHealth -= 1;
 
             if (_currentHealth <= 0)
-                Destroy(gameObject);
+            {
+                _animator.SetBool("IsDead", true);
+                _target.EnemyKilled();
+                Destroy(gameObject, 1f);
+            }
+            else
+            {
+                _animator.SetTrigger("GotHit");
+            }
+        }
+
+        if (collision.collider.tag == "Player")
+        {
+            _canAttack = true;
         }
     }
 
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            _canAttack = false;
+        }
+    }
 
+    private void Attack()
+    {
+        _attackTimer = 0f;
+        _animator.SetTrigger("Attack");
+        _target.GotHit(1);
+    }
 }
--- Assets/ShootingGame/Scripts/PlayerController.cs
+++ Assets/ShootingGame/Scripts/PlayerController.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SocialPlatforms.Impl;
 
 namespace ShooterGame
 {
     public class PlayerController : MonoBehaviour
     {
         private Rigidbody _rigidBody;
+        private Animator _animator;
 
         [SerializeField]
         private float _moveSpeed;
@@ -26,10 +27,19 @@ namespace ShooterGame
         private float _rateOfFire;
         private float _fireTimer;
 
+        [SerializeField]
+        private float _maxHealth;
+        private float _currentHealth;
+
+        private int _score;
+
         private void Start()
         {
+            _animator = GetComponent<Animator>();
             _rigidBody = GetComponent<Rigidbody>();
             _fireTimer = _rateOfFire;
+            _currentHealth = _maxHealth;
+            _score = 0;
         }
 
         private void Update()
@@ -56,6 +66,7 @@ namespace ShooterGame
         {
             GameObject go = Instantiate(_bulletPrefab, _muzzleTransform.position, _muzzleTransform.rotation);
             _fireTimer = 0f;
+
         }
 
         private void FixedUpdate()
@@ -67,6 +78,28 @@ namespace ShooterGame
             playerVelocity.y = _rigidBody.velocity.y;
 
             _rigidBody.velocity = playerVelocity;
+
+            if (_rigidBody.velocity.sqrMagnitude > 0.1f)
+                _animator.SetBool("IsWalking", true);
+            else
+                _animator.SetBool("IsWalking", false);
+        }
+
+        public void GotHit(float damage)
+        {
+            
+            _currentHealth -= damage;
+            Debug.Log("hit:" + damage + " health: " + _currentHealth);
+            if (_currentHealth <= 0)
+                _animator.SetBool("IsDead", true);
+            else
+                _animator.SetTrigger("GotHit");
+
+        }
+
+        public void EnemyKilled()
+        {
+            _score += 1;
         }
 
     }
